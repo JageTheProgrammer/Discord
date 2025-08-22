9b607012bd16b6c2e541953a4a18231e100a74a2
Jaakko Maatiala <131030750+JageTheProgrammer@users.noreply.github.com>
2025-08-22 06:00:07 +0300

    Create botinfo.js

---
+++ commands/fun/botinfo.js
@@ -0,0 +1,16 @@
+// commands/utils/botinfo.js
+import { SlashCommandBuilder } from 'discord.js';
+import pkg from '../../package.json' assert { type: 'json' };
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('botinfo')
+    .setDescription('Get info about the bot'),
+
+  async execute(interaction) {
+    const { client } = interaction;
+    await interaction.reply(
+      `🤖 **${client.user.tag}**\n📅 Created: ${client.user.createdAt.toDateString()}\n📝 Version: ${pkg.version}`
+    );
+  },
+};
